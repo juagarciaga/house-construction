bae41fc92ad579d1d74ecd94934d83473ac8cd31
Juliana García <julianagarcia2009@gmail.com>
2025-03-05 20:26:43 +0000

    Update Home page links with improved styling and accessibility

--- house-construction/src/app/page.tsx
+++ house-construction/src/app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Link href="/family" className="btn mt-4">
+        <Link href="/family" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           Family Expenses
         </Link>
 
-        <Link href="/construction">
+        <Link href="/construction" className=" rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
           Construction
         </Link>
 
