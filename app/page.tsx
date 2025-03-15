b403bbb36b4e94d987948d2fcd1b60928913fb00
Juliana García <julianagarcia2009@gmail.com>
2025-03-15 18:31:40 +0000

    Add Amplify authentication and data resources; configure backend and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 'use client'
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { Amplify } from 'aws-amplify';
+import outputs from '../amplify_outputs.json';
 import DashboardLayout from "./dashboard.layout";
-import LoginPage from "./login/page";
 
+Amplify.configure(outputs);
 
 export default function Home() {
-  const isLooged = false;
   return (
-    <>
-
-      {isLooged ? <DashboardLayout> <h1>Hello World!!</h1> </DashboardLayout> : <LoginPage />}
-    </>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <>
+          <button onClick={signOut}>Sign out</button>
+          <p>Hello, {user?.username}</p>
+          <DashboardLayout> <h1>Hello World!!</h1> </DashboardLayout>
+        </>
+      )}
+    </Authenticator>
   );
 }
